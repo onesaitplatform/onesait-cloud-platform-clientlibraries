393ae6feb444c0facc8e8ebd4ecf934b7f8819d0
ebustos <ebustos@indra.es>
2019-05-21 16:56:06 +0200

    add __version__ to modules

--- python-client/onesaitplatform/enum/__init__.py
+++ python-client/onesaitplatform/enum/__init__.py
@@ -2,3 +2,4 @@ from .querytype import QueryType
 from .restmethods import RestMethods
 from .resprotocols import RestProtocols
 from .restheaders import RestHeaders
+__version__ = "1.1.2"
--- python-client/onesaitplatform/iotbroker/__init__.py
+++ python-client/onesaitplatform/iotbroker/__init__.py
@@ -1,2 +1,3 @@
 from .iotbrokerclient import IotBrokerClient
 from .iotbrokerclient import IotBrokerClient as DigitalClient
+__version__ = "1.1.2"
