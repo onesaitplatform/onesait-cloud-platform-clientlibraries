9112f05cb5cefe292056c5d0b1bf75188c7f917a
Benjamín Ramírez <bjramirez@minsait.com>
2021-04-07 09:28:16 +0200

    Remove module information from audit

--- python-client/onesaitplatform/model/modelservice.py
+++ python-client/onesaitplatform/model/modelservice.py
@@ -51,13 +51,12 @@ class AuditClient(object):
         self.headers = headers
 
     def report(
-        self, module=None, message=None, ontology=None, operation_type=None,
+        self, message=None, ontology=None, operation_type=None,
         other_type=None, result_operation=None, type_=None
         ):
         now = datetime.now()
         date_formated = now.strftime(DATETIME_PATTERN)
         data = [{
-            "module": module,
             "formatedTimeStamp": date_formated,
             "message": message,
             "ontology": ontology,
